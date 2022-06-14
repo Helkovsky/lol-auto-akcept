d6cd5558569d3da7946250bb55945442caf8e539
Helkovsky <44656149+Kacperk1234567890@users.noreply.github.com>
2022-06-14 03:04:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import pyautogui
+import time
+import os
+tab = ["/", "—", "\\", "|", "—"]
+i = 0
+
+while True:
+    os.system('cls')
+    print(f"Wyszukiwanie gry {tab[i]}")
+    if not pyautogui.locateOnScreen("img1.png") is None:
+        pyautogui.click("img1.png")
+        print("Znaleziono mecz!")
+        time.sleep(10)
+
+    i = i + 1
+    if i == 4:
+        i = 0
+    time.sleep(5)
+
+print(pyautogui.locateOnScreen("img.png"))
+
+input()
